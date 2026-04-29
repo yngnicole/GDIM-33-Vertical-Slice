fafd8dc16d7a5f121b4adf031eb302e6fb811db8
yngnicole <nicoley5@uci.edu>
2026-04-28 21:10:57 -0700

    s

--- GDIM 33 Vertical Slice/Assets/Scripts/TextUi.cs
+++ GDIM 33 Vertical Slice/Assets/Scripts/TextUi.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using TMPro.EditorUtilities;
+//using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class TextUi : MonoBehaviour
