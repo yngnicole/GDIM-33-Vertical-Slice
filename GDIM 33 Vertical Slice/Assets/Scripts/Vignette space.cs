738d6befc7fbd4b241bba0504a0ea6ad5308a000
yngnicole <nicoley5@uci.edu>
2026-06-10 19:47:39 -0700

    s

--- GDIM 33 Vertical Slice/Assets/Scripts/Vignette space.cs
+++ GDIM 33 Vertical Slice/Assets/Scripts/Vignette space.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Vignettespace : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Material vignetteMaterial;
+    [SerializeField] private string shaderPropertyName = "_Vignette_Intensity";
+    [SerializeField] private float _fadeSpeed = 4f;
+    [SerializeField] private float _maxIntensity = 1f;
+
+    private float _targetIntensity = 0f;
+    private float _currentIntensity = 0f;
     void Start()
     {
-        
+        vignetteMaterial.SetFloat(shaderPropertyName, 0f);
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        _currentIntensity = Mathf.MoveTowards(_currentIntensity, _targetIntensity, _fadeSpeed * Time.deltaTime);
+        vignetteMaterial.SetFloat(shaderPropertyName, _currentIntensity);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.CompareTag("Player"))
+        {
+            _targetIntensity = _maxIntensity;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            _targetIntensity = 0f;
+        }
+    }
+
 }
